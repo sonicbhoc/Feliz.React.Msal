63fb6871418558e86b6c26aa2fbeecca965476f3
cchristie <cchristie@generaldigital.com>
2023-01-03 15:58:22 -0500

    Add Build command to format code using Fantomas

--- Build/build.fs
+++ Build/build.fs
@@ -10,10 +10,10 @@ let mutable dotNetOptions = fun _ -> DotNet.Options.Create()
 
 let slnDir = __SOURCE_DIRECTORY__ @@ "../" |> Path.getFullName
 let projName = "Feliz.React.Msal"
-let projDir =  slnDir @@ projName
+let projDir = slnDir @@ projName
 let paketLockFile = slnDir @@ "paket.lock"
 
-let printRes (operation : string) (res : ProcessResult) =
+let printRes (operation: string) (res: ProcessResult) =
     match res with
     | res when res.ExitCode = 0 -> ()
     | res ->
@@ -21,54 +21,58 @@ let printRes (operation : string) (res : ProcessResult) =
         failwith $"{operation} failed: {System.Environment.NewLine}{res.Errors |> concat}{System.Environment.NewLine}"
 
 let initTargets () =
-    let installDeps = BuildTask.create "InstallDependencies" [] {
-        dotNetOptions <- DotNet.install (fun opts ->
-            { opts with
-                Channel = DotNet.CliChannel.LTS})
+    let installDeps =
+        BuildTask.create "InstallDependencies" [] {
+            dotNetOptions <- DotNet.install (fun opts -> { opts with Channel = DotNet.CliChannel.LTS })
 
-        DotNet.exec dotNetOptions "tool" "restore" |> printRes "Tool restore"
+            DotNet.exec dotNetOptions "tool" "restore"
+            |> printRes "Tool restore"
 
-        let paketCmd =
-            match File.exists paketLockFile with
-            | true -> "restore"
-            | false -> "install"
+            let paketCmd =
+                match File.exists paketLockFile with
+                | true -> "restore"
+                | false -> "install"
 
-        DotNet.exec dotNetOptions "paket" paketCmd |> printRes "Paket install"
+            DotNet.exec dotNetOptions "paket" paketCmd
+            |> printRes "Paket install"
 
-        DotNet.exec dotNetOptions "femto" $"{projDir} --resolve"  |> printRes "Femto package resolution"
-    }
+            DotNet.exec dotNetOptions "femto" $"{projDir} --resolve"
+            |> printRes "Femto package resolution"
+        }
 
-    let clean = BuildTask.create "Clean" [] {
-        !! (projDir @@ "/**/bin")
-        ++ (projDir @@ "/**/obj")
-        |> Shell.cleanDirs
+    let clean =
+        BuildTask.create "Clean" [] {
+            !!(projDir @@ "/**/bin") ++ (projDir @@ "/**/obj")
+            |> Shell.cleanDirs
 
-        !! (projDir @@ "/**/*.nupkg")
-        |> Seq.iter (Shell.rm)
-    }
+            !!(projDir @@ "/**/*.nupkg")
+            |> Seq.iter (Shell.rm)
+        }
 
     let build config =
-         DotNet.build (fun opts ->
-            { opts with
-                Configuration = config
-            }
-         ) (projDir @@ $"{projName}.fsproj")
+        DotNet.build (fun opts -> { opts with Configuration = config }) (projDir @@ $"{projName}.fsproj")
 
-    let buildDebug = BuildTask.create "BuildDebug" [installDeps.IfNeeded; clean.IfNeeded] {
-        build DotNet.Debug
-    }
+    let buildDebug =
+        BuildTask.create "BuildDebug" [ installDeps.IfNeeded; clean.IfNeeded ] { build DotNet.Debug }
 
-    let buildRelease = BuildTask.create "BuildRelease" [installDeps.IfNeeded; clean.IfNeeded] {
-        build DotNet.Release
-    }
+    let buildRelease =
+        BuildTask.create "BuildRelease" [ installDeps.IfNeeded; clean.IfNeeded ] { build DotNet.Release }
 
-    let pack = BuildTask.create "Pack" [buildRelease] {
-        DotNet.exec id "paket" $"pack --symbols {projDir}" |> printRes "Pack"
-    }
+    let pack =
+        BuildTask.create "Pack" [ buildRelease ] {
+            DotNet.exec id "paket" $"pack --symbols {projDir}"
+            |> printRes "Pack"
+        }
 
-    let _all = BuildTask.createEmpty "All" [installDeps; clean; pack]
+    let _all = BuildTask.createEmpty "All" [ installDeps; clean; pack ]
 
-    let defaultBuild = BuildTask.createEmpty "Build" [buildDebug]
+    let defaultBuild = BuildTask.createEmpty "Build" [ buildDebug ]
+
+    let format =
+        BuildTask.create "Format" [ installDeps.IfNeeded ] {
+            DotNet.exec id "fantomas" $"{slnDir} -r"
+            |> printRes "Fantomas"
+        }
 
     _all
 
@@ -79,4 +83,5 @@ let main args =
     |> FakeExecutionContext.Create false (__SOURCE_DIRECTORY__ @@ __SOURCE_FILE__)
     |> Fake
     |> setExecutionContext
+
     BuildTask.runOrDefaultApp (initTargets ())
